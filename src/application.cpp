4c2faae74d470aab2b84f0d004e892f8c77bf3c7
SacreDeBirmanie <sebful44@hotmail.fr>
2014-04-14 22:25:26 +0200

    ajout fichier

--- src/application.cpp
+++ src/application.cpp
@@ -10,36 +10,36 @@ using namespace std;
 
 
 int main(){
-	
+
 	cout<<"Bievenue utilisateur"<<endl;
 	cout<<"Pour utliser un de vos documents, vous devez prealablement le placer dans le repertoire res/Documents du projet yolo-batman"<<endl;
-	
+
 	cout<<"voici la liste des documents presents :"<<endl;
 		DIR * rep = opendir("../res/Documents");
-		
+
 		if (rep == NULL)
 			cout<<"erreur, le dossier Documents n existe plus, impossible de faire des traitements"<<endl;
 		else{
 			struct dirent * ent;
-			 
+
 			while ((ent = readdir(rep)) != NULL)
 			{
 				printf("%s\n", ent->d_name);
 			}
-			 
+            cout<<endl;
 			closedir(rep);
 		}
-	
+
 	string nomFichier;
 	cout<<"veuillez entrer le nom du fichier que vous voulez utiliser : "<<endl;
 	cin>>nomFichier;
-	
-	
-			
-	
+
+
+
+
     Dictionnaire<string> dico;
-	
-	
+
+
 	string cheminFichier = "../res/Documents/";
 	cheminFichier = cheminFichier + nomFichier;
 	ifstream fichier(cheminFichier.c_str());
