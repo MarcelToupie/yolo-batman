43ff1ae9a4d47849f5c8159d3c7687076c05cf13
Antoine BOUTRUCHE <E115325D@S040V7pc09.ensinfo.sciences.univ-nantes.prive>
2014-04-24 16:51:48 +0200

    Arbre caca

--- src/DicoArbre/arbre.cpp
+++ src/DicoArbre/arbre.cpp
@@ -1,20 +1,40 @@
-bool estFeuille(Arbre a){
+Arbre<T,Association>::Arbre(T etq,assoation assoc){
+		this->etq=etq;
+		associe = assoc;
+}
+
+void Arbre<T,Association>::modifierAssocie(Assocation assoc){
+		this->associe = assoc;
+}
+template<typename T, typename Association>
+bool Arbre<T, Association>::estFeuille(Arbre a){
 	return nbFils==0;
 }
-T etq(Arbre a){
+
+template<typename T, typename Association>
+T Arbre<T, Association>::etq(Arbre a){
 	return etq;
 }
-Arbre renvoiFils(T etq){
+
+template<typename T, typename Association>
+Arbre Arbre<T, Association>::renvoiFils(T etq){
 	Arbre fils = NULL;
 	for(int i=0; i<nbFils; i++){
-		if(lesFils[i].etq) fils = tab[i]; 
+		if(lesFils[i].etq = etq) fils = tab[i]; 
 	}
 	
 	return fils;
 }
-Arbre ajouterFils(T etq){
-	lesFils[nbFils+1]<-new Arbre(a);
+
+template<typename T, typename Association>
+Arbre Arbre<T, Association>::ajouterFils(T etq,Association assoc){
+	lesFils[nbFils] = new Arbre(etq,assoc);
 	++nbFils;
+}
+
+template<typename T, typename Association>
+Association Arbre<T, Association>::renvoiAssocie(){
+	return associe;
 }
 
 
--- src/DicoArbre/arbre.hpp
+++ src/DicoArbre/arbre.hpp
@@ -1,17 +1,23 @@
+
+template<typename T, typename Association>
+
 class Arbre{
-	
-	private{
+
+
+	private:
 		T etq;
 		Arbre lesFils[15];
-		int nbFils;
-	}
+		int nbFils = 0;
+		Association associe;
+		
+	
 
-	public{
-		bool estFeuille();
-		T etq();
-		Arbre renvoiFils(T etq);
-		Arbre ajouterFils(T etq);
-		}
+	public:
+		bool estFeuille();//retourne vrai si l'arbre est une feuille
+		T etq();//retourne l'étiquette de l'arbre
+		Arbre renvoiFils(T etq);//renvoi les fils de l'arbre
+		Arbre ajouterFils(T etq,Association assoc);//ajoute un fils à l'arbre
+		Association renvoiAssocie();//renvoi l'objet associé à l'étiquette
 		
 }
 
